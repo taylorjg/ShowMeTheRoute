03c652aaaa3eac13ff38a9ebe16f151b914636ae
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-09-30 10:49:06 +0100

    Allow apiKey to be specified in the query string

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 $(document).ready(() => {
     const $apiKey = $('#apiKey');
     const $submitBtn = $('#submitBtn');
-    $submitBtn.click(function(e) {
+
+    $apiKey.val(queryStringToMap()['apiKey']);
+
+    $submitBtn.click(function (e) {
         e.preventDefault();
         const apiKey = $apiKey.val();
         const src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
         $('head').append($('<script>', { src }));
     });
 });
+
+function queryStringToMap() {
+    const queries = {};
+    $.each(document.location.search.substr(1).split('&'), function (_, q) {
+        const bits = q.split('=');
+        queries[bits[0]] = bits[1];
+    });
+    return queries;
+}
 
 function initMap() {
     console.log(`init()`);
@@ -18,7 +30,7 @@ function initMap() {
         destination: 'Cake Solutions, Houldsworth Street, Stockport',
         travelMode: 'DRIVING'
     };
-    directionsService.route(request, function(response, status) {
+    directionsService.route(request, function (response, status) {
         console.dir(arguments);
         response.routes[0].overview_path.forEach((p, index) => {
             console.log(`[${index}] lat: ${p.lat()}; lng: ${p.lng()}`);
