e866d7bff553b07635ff52f8718b0e440dc11a05
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-10-01 00:23:22 +0100

    Auto advance using an interval timer

--- index.js
+++ index.js
@@ -18,20 +18,26 @@ queryStringToMap = () => {
 }
 
 function initMap() {
+
     const directionsService = new google.maps.DirectionsService();
+
     const request = {
         origin: '29 Brookfield Ave, Manchester, England',
         destination: 'Cake Solutions, Houldsworth Street, Stockport',
         travelMode: 'DRIVING'
     };
+
     directionsService.route(request, function (response, status) {
+
         const numPositions = response.routes[0].overview_path.length;
         let positionIndex = 0;
         let nextHeading = null;
+        let timer = null;
         const position1 = response.routes[0].overview_path[positionIndex];
         const position2 = response.routes[0].overview_path[positionIndex + 1];
         const initialHeading = google.maps.geometry.spherical.computeHeading(position1, position2);
         const mapDiv = document.getElementById('map');
+
         const panorama = new google.maps.StreetViewPanorama(
             mapDiv, {
                 position: position1,
@@ -45,20 +51,38 @@ function initMap() {
                 fullscreenControl: false,
                 motionTrackingControl: false,
                 linksControl: false,
-                enableCloseButton: false
+                enableCloseButton: false,
+                showRoadLabels: false
             });
+
+        const goToNextLocation = () => {
+            if (++positionIndex < numPositions) {
+                showPositionIndex(positionIndex);
+            }
+        };
+
+        $('#startBtn').click(() => {
+            const id = setInterval(goToNextLocation, 1000);
+            timer = { id };
+        });
+
+        $('#stopBtn').click(() => {
+            if (timer) {
+                clearInterval(timer.id);
+                timer = null;
+            }
+        });
+
         panorama.addListener('position_changed', () => {
-            console.log(`[position_changed] panorama.position: ${JSON.stringify(panorama.position)}; panorama.pov: ${JSON.stringify(panorama.pov)}`);
             if (nextHeading) {
                 setTimeout(() => {
-                    console.log(`new heading: ${JSON.stringify(nextHeading.heading)}`);
                     const pov = panorama.pov;
                     pov.heading = nextHeading.heading;
                     panorama.setPov(pov);
                 }, 100);
             }
         });
-        const src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=geometry&callback=initMap`;
+
         const showPositionIndex = positionIndex => {
             const position1 = response.routes[0].overview_path[positionIndex];
             if (positionIndex + 1 < numPositions) {
@@ -72,11 +96,5 @@ function initMap() {
                 panorama.setPosition(position1);
             }
         };
-        const $nextLocationBtn = $('#nextLocationBtn');
-        $nextLocationBtn.click(() => {
-            if (++positionIndex < numPositions) {
-                showPositionIndex(positionIndex);
-            }
-        });
     });
 };
