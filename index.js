18294ca523483749a433f3e635d1c77ca8214eca
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-09-30 18:17:27 +0100

    Improve appearance by changing heading after a delay

--- index.js
+++ index.js
@@ -27,15 +27,16 @@ function initMap() {
     directionsService.route(request, function (response, status) {
         const numPositions = response.routes[0].overview_path.length;
         let positionIndex = 0;
+        let nextHeading = null;
         const position1 = response.routes[0].overview_path[positionIndex];
         const position2 = response.routes[0].overview_path[positionIndex + 1];
-        const heading = google.maps.geometry.spherical.computeHeading(position1, position2);
-        const mapDiv = $('#map')[0];
+        const initialHeading = google.maps.geometry.spherical.computeHeading(position1, position2);
+        const mapDiv = document.getElementById('map');
         const panorama = new google.maps.StreetViewPanorama(
             mapDiv, {
                 position: position1,
                 pov: {
-                    heading,
+                    heading: initialHeading,
                     pitch: 0
                 },
                 panControl: false,
@@ -46,18 +47,28 @@ function initMap() {
                 linksControl: false,
                 enableCloseButton: false
             });
+        panorama.addListener('position_changed', () => {
+            console.log(`[position_changed] panorama.position: ${JSON.stringify(panorama.position)}; panorama.pov: ${JSON.stringify(panorama.pov)}`);
+            if (nextHeading) {
+                setTimeout(() => {
+                    console.log(`new heading: ${JSON.stringify(nextHeading.heading)}`);
+                    const pov = panorama.pov;
+                    pov.heading = nextHeading.heading;
+                    panorama.setPov(pov);
+                }, 100);
+            }
+        });
         const src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=geometry&callback=initMap`;
         const showPositionIndex = positionIndex => {
             const position1 = response.routes[0].overview_path[positionIndex];
             if (positionIndex + 1 < numPositions) {
                 const position2 = response.routes[0].overview_path[positionIndex + 1];
                 const heading = google.maps.geometry.spherical.computeHeading(position1, position2);
+                nextHeading = { heading };
                 panorama.setPosition(position1);
-                const pov = panorama.pov;
-                pov.heading = heading;
-                panorama.setPov(pov);
             }
             else {
+                nextHeading = null;
                 panorama.setPosition(position1);
             }
         };
