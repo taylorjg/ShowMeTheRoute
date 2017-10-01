28461d718895d53f3b60659490b44e2df7cd8f2a
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-10-01 22:49:11 +0100

    Allow origin and destination to be specified

--- client/index.js
+++ client/index.js
@@ -3,7 +3,13 @@
 $(document).ready(() => {
 
     const $apiKey = $('#apiKey');
-    $apiKey.val(queryStringToMap().get('apiKey'));
+    const $origin = $('#origin');
+    const $destination = $('#destination');
+
+    const qsmap = queryStringToMap();
+    $apiKey.val(qsmap.get('apiKey'));
+    $origin.val(qsmap.get('origin'));
+    $destination.val(qsmap.get('destination'));
 
     const onSubmit = e => {
         e.preventDefault();
@@ -25,99 +31,110 @@ window.initMap = function () {
     const ADVANCE_TO_NEXT_STEP_INTERVAL = 1000;
     const ADJUST_HEADING_DELAY = 100;
 
+    const mapDiv = document.getElementById('map');
+    const panorama = new google.maps.StreetViewPanorama(
+        mapDiv, {
+            panControl: false,
+            zoomControl: false,
+            addressControl: false,
+            fullscreenControl: false,
+            motionTrackingControl: false,
+            linksControl: false,
+            enableCloseButton: false,
+            showRoadLabels: false
+        });
     const directionsService = new google.maps.DirectionsService();
 
-    const request = {
-        origin: '29 Brookfield Ave, Manchester, England',
-        destination: 'Cake Solutions, Houldsworth Street, Stockport',
-        travelMode: 'DRIVING'
+    let path = [];
+    let fullPath = [];
+    let numPositions = 0;
+    let positionIndex = 0;
+    let nextHeading = null;
+    let timer = null;
+
+    panorama.addListener('position_changed', () => {
+        if (nextHeading) {
+            setTimeout(() => {
+                const pov = panorama.pov;
+                pov.heading = nextHeading.heading;
+                panorama.setPov(pov);
+            }, ADJUST_HEADING_DELAY);
+        }
+    });
+
+    const showPositionIndex = positionIndex => {
+        const position1 = path[positionIndex];
+        panorama.setPosition(position1);
+        if (positionIndex + 1 < numPositions) {
+            const position2 = path[positionIndex + 1];
+            const heading = google.maps.geometry.spherical.computeHeading(position1, position2);
+            nextHeading = { heading };
+        }
+        else {
+            nextHeading = null;
+        }
     };
 
-    directionsService.route(request, function (response, status) {
-
-        if (status !== 'OK') {
-            console.warn(`DirectionsService returned status: ${status}`);
-            return;
+    const advance = () => {
+        if (++positionIndex < numPositions) {
+            showPositionIndex(positionIndex);
         }
+        else {
+            pause();
+        }
+    };
 
-        const steps = response.routes[0].legs[0].steps;
-        steps.forEach((step, index) => {
-            console.log(`[step ${index}] instructions: ${step.instructions}; path length: ${step.path.length}`);
+    const loadInitialPosition = () => {
+        positionIndex = 0;
+        const position = path[positionIndex];
+        panorama.setPosition(position);
+        const heading = google.maps.geometry.spherical.computeHeading(fullPath[0], fullPath[1]);
+        nextHeading = { heading };
+    };
+
+    const play = () => {
+        const id = setInterval(advance, ADVANCE_TO_NEXT_STEP_INTERVAL);
+        timer = { id };
+    };
+
+    const pause = () => {
+        if (timer) {
+            clearInterval(timer.id);
+            timer = null;
+        }
+    };
+
+    const reset = () => {
+        loadInitialPosition();
+    };
+
+    const showRoute = e => {
+        e.preventDefault();
+        const $origin = $('#origin');
+        const $destination = $('#destination');
+        const request = {
+            origin: $origin.val(),
+            destination: $destination.val(),
+            travelMode: 'DRIVING'
+        };
+
+        directionsService.route(request, function (response, status) {
+            if (status !== 'OK') {
+                console.warn(`DirectionsService returned status: ${status}`);
+                return;
+            }
+            path = response.routes[0].overview_path;
+            numPositions = path.length;
+
+            const steps = response.routes[0].legs[0].steps;
+            const paths = steps.map(step => step.path);
+            fullPath = [].concat(...paths);
+            loadInitialPosition();
         });
-        const paths = steps.map(step => step.path);
-        const path = [].concat(...paths);
+    };
 
-        const numPositions = path.length;
-        let positionIndex = 0;
-        let nextHeading = null;
-        let timer = null;
-        const position1 = path[positionIndex];
-        const position2 = path[positionIndex + 1];
-        const initialHeading = google.maps.geometry.spherical.computeHeading(position1, position2);
-        const mapDiv = document.getElementById('map');
-
-        const panorama = new google.maps.StreetViewPanorama(
-            mapDiv, {
-                position: position1,
-                pov: {
-                    heading: initialHeading,
-                    pitch: 0
-                },
-                panControl: false,
-                zoomControl: false,
-                addressControl: false,
-                fullscreenControl: false,
-                motionTrackingControl: false,
-                linksControl: false,
-                enableCloseButton: false,
-                showRoadLabels: false
-            });
-
-        panorama.addListener('position_changed', () => {
-            if (nextHeading) {
-                setTimeout(() => {
-                    const pov = panorama.pov;
-                    pov.heading = nextHeading.heading;
-                    panorama.setPov(pov);
-                }, ADJUST_HEADING_DELAY);
-            }
-        });
-
-        const showPositionIndex = positionIndex => {
-            const position1 = path[positionIndex];
-            panorama.setPosition(position1);
-            if (positionIndex + 1 < numPositions) {
-                const position2 = path[positionIndex + 1];
-                const heading = google.maps.geometry.spherical.computeHeading(position1, position2);
-                nextHeading = { heading };
-            }
-            else {
-                nextHeading = null;
-            }
-        };
-
-        const advance = () => {
-            if (++positionIndex < numPositions) {
-                showPositionIndex(positionIndex);
-            }
-            else {
-                stopAdvancing();
-            }
-        };
-
-        const startAdvancing = () => {
-            const id = setInterval(advance, ADVANCE_TO_NEXT_STEP_INTERVAL);
-            timer = { id };
-        };
-
-        const stopAdvancing = () => {
-            if (timer) {
-                clearInterval(timer.id);
-                timer = null;
-            }
-        };
-
-        $('#startBtn').click(startAdvancing);
-        $('#stopBtn').click(stopAdvancing);
-    });
+    $('#showRouteBtn').click(showRoute);
+    $('#playBtn').click(play);
+    $('#pauseBtn').click(pause);
+    $('#resetBtn').click(reset);
 };
