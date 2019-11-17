2d177609f07a5b17ae0d61ba5f4b65c3ff7af5a9
Administrator <jonathan.taylor.programmer@gmail.com>
2019-11-17 19:13:58 +0000

    Prep for using new Google api key

--- webpack.config.js
+++ webpack.config.js
@@ -8,27 +8,26 @@ const packageJson = require('./package.json');
 const serverPublic = path.join(__dirname, 'server', 'public');
 
 module.exports = {
-    entry: [
-        './client/index.js'
-    ],
-    output: {
-        path: serverPublic,
-        filename: 'bundle.js',
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            { context: './client', from: '*.html' },
-            { context: './client', from: '*.css' }
-        ]),
-        new HtmlWebpackPlugin({
-            template: './client/index.html',
-            inject: false,
-            version: packageJson.version,
-            googleApiKey: process.env.GOOGLE_API_KEY
-        })
-    ],
-    devtool: 'source-map',
-    devServer: {
-        contentBase: serverPublic
-    }
+  // mode: 'production',
+  entry: './client/index.js',
+  output: {
+    path: serverPublic,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { context: './client', from: '*.html' },
+      { context: './client', from: '*.css' }
+    ]),
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      inject: false,
+      version: packageJson.version,
+      googleApiKey: process.env.GOOGLE_API_KEY
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: serverPublic
+  }
 };
